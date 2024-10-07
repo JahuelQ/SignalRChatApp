05df4f9de02fe6d98086293c36b7bfd35d74e0de
Joel Q <JoelQvist@live.se>
2024-10-07 14:43:59 +0200

    Initial server setup

---
+++ ChatApp.Server/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ChatApp.Shared.Models;
+
+namespace ChatApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage (ChatMessage chatMessage)
+        {
+            // Sanitize message to prevent XSS
+            chatMessage.Message = System.Net.WebUtility.HtmlDecode (chatMessage.Message);
+
+            await Clients.All.SendAsync("RecieveMessage", chatMessage);
+        }
+    }
+}
--- ChatApp.Server/Program.cs
+++ ChatApp.Server/Program.cs
@@ -1,3 +1,5 @@
+using ChatApp.Server.Hubs;
+
 namespace ChatApp.Server
 {
     public class Program
@@ -5,9 +7,30 @@ namespace ChatApp.Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add SignalR
+            builder.Services.AddSignalR();
+
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenLocalhost(7181, listenOptions =>
+                {
+                    listenOptions.UseHttps(); // HTTPS on localhost:7181
+                });
+                options.ListenAnyIP(5140); // HTTP on any IP at port 5140
+            });
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.MapHub<ChatHub>("/chathub");
 
             app.Run();
         }
